481446663e4186ea1ae5d9f18b378eaedf6923e0
MadeleineHarbom <madeleine.harbom@gmail.com>
2020-11-03 20:21:21 +0100

    Static stuff

--- lulz/views.py
+++ lulz/views.py
@@ -4,9 +4,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg']
+
 
 def random_lulz(request):
-    images = ['IMG_0422.jpg', 'IMG_0427.jpg', 'IMG_0479.jpg', 'IMG_0480.jpg', 'IMG_0482.jpg']
     imgage = random.choice(images)
     template = render(request, "index.html", {'image_url': imgage})
+    return HttpResponse(template)
+
+
+def comic(request, comic_id):
+    template = render(request, 'comic.html', {
+        'previous_url': reverse('comic', kwargs={'comic_id': comic_id - 1 if comic_id > 0 else len(images) - 1}),
+        'next_url': reverse('comic', kwargs={'comic_id': comic_id + 1 if comic_id < len(images) - 1 else 0}),
+        'comic_url': images[comic_id]
+    })
     return HttpResponse(template)
--- ykcd/urls.py
+++ ykcd/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from lulz.views import random_lulz
+from lulz.views import random_lulz, comic
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', random_lulz)
+    path('', random_lulz),
+    path('comic/<int:comic_id>', comic, name='comic')
 ]
