c4f595fcefc288bde33831e079bae3c4068891bd
MadeleineHarbom <madeleine.harbom@gmail.com>
2021-01-01 19:35:54 +0100

    15

--- lulz/views.py
+++ lulz/views.py
@@ -11,7 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 from lulz.models import Comment
 
 images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg',
-          '11.jpg', '12.jpg', '13.jpg', '14.jpg']
+          '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg']
 
 
 def random_lulz(request):
