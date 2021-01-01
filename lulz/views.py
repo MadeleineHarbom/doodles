50b25212ae0ead1c5e8ded72c409d53f32b50941
MadeleineHarbom <madeleine.harbom@gmail.com>
2021-01-01 19:21:03 +0100

    Static stuff

--- lulz/views.py
+++ lulz/views.py
@@ -10,7 +10,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from lulz.models import Comment
 
-images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg']
+images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg',
+          '11.jpg', '12.jpg', '13.jpg', '14.jpg']
 
 
 def random_lulz(request):
